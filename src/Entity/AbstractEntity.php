2a9ddd714438fd2015ff82cfb566ff6b2da4999b
parsilver <magician_canobi@hotmail.com>
2022-05-27 15:13:06 +0700

    Apply JsonSerializable to Entity

--- src/Entity/AbstractEntity.php
+++ src/Entity/AbstractEntity.php
@@ -2,8 +2,19 @@
 
 namespace ZipzoftInterview\ShoppingCart\Entity;
 
-abstract class AbstractEntity
+use JsonSerializable;
+use ArrayAccess;
+
+abstract class AbstractEntity implements JsonSerializable, ArrayAccess
 {
+
+    public function __construct(array $data = [])
+    {
+        foreach ($data as $key => $value) {
+            $this->$key = $value;
+        }
+    }
+
     /**
      * Initial from an array of data
      */
@@ -15,5 +26,68 @@ abstract class AbstractEntity
         }
 
         return $entity;
+    }
+
+    /**
+     * Convert to an array
+     */
+    public function toArray()
+    {
+        $array = [];
+        foreach ($this as $key => $value) {
+            $array[$key] = $value;
+        }
+
+        return $array;
+    }
+
+    /**
+     * Convert to JSON
+     */
+    public function jsonSerialize()
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * Whether a offset exists
+     */
+    public function offsetExists($offset): bool
+    {
+        return isset($this->$offset);
+    }
+
+    /**
+     * Offset to retrieve
+     */
+    public function offsetGet($offset)
+    {
+        return $this->$offset;
+    }
+
+    /**
+     * Offset to set
+     */
+    public function offsetSet($offset, $value): void
+    {
+        $this->$offset = $value;
+    }
+
+    /**
+     * Offset to unset
+     */
+    public function offsetUnset($offset): void
+    {
+        unset($this->$offset);
+    }
+
+
+    public function __get($name)
+    {
+        if (property_exists($this, $name)) {
+            return $this->$name;
+        }
+
+        return null;
     }
 }
